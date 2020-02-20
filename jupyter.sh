ef4f5816b584ebf08946c2f9866b17238a3e64eb
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-02-20 21:36:27 +0300

    no jupyter.sh

--- jupyter.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-jupyter notebook --no-browser --ip=0.0.0.0 --allow-root     --NotebookApp.token= --notebook-dir=/project/notebooks             --NotebookApp.default_url=/project/notebooks/demo.ipynb
